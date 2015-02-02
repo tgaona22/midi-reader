865d5f7b8b41b937d19e57ba774f551829b40d09
tgaona22 <gaonat@duq.edu>
2015-02-01 21:15:09 -0500

    Fixed interpretation of bytes bug.

--- MidiReader.java
+++ MidiReader.java
@@ -13,19 +13,23 @@ public class MidiReader {
     }
 
     public void readHeaderChunk() {
-	char headID[] = {'M', 'T', 'h', 'd'};
-	for (int i = 0; i < 4; i++) {
-	    if (input.readChar() != headID[i])
-		System.err.println("Error reading header chunk. Not a valid Midi file.");
+	try {
+	    char headID[] = {'M', 'T', 'h', 'd'};
+	    for (int i = 0; i < 4; i++) {
+		if ((char)input.readByte() != headID[i])
+		    System.err.println("Error reading header chunk. Not a valid Midi file.");
+	    }
+	    headerChunkLength = input.readInt();
+	    fileFormat = input.readShort();
+	    trackCount = input.readShort();
+	    timeDivision = input.readShort();
+	} catch (IOException e) {
+	    System.err.println(e.toString());
 	}
-	headerChunkLength = input.readInt();
-	fileFormat = input.readShort();
-	trackCount = input.readShort();
-	timeDivision = input.readShort();
     }
     public void printHeaderInformation() {
 	System.out.println("File Format: " + fileFormat);
 	System.out.println("Number of track chunks: " + trackCount);
-	System.out.println("Time division: " + timeDivision);
+	System.out.println("Time division: " + timeDivision + " ticks per beat.");
     }
 }
