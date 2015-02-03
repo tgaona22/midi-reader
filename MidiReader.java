e6dac8d7cd2730ba6fa930f5db32d593fc7721a3
tgaona22 <gaonat@duq.edu>
2015-02-02 19:49:25 -0500

    Changed readHeaderChunk() to return boolean

--- MidiReader.java
+++ MidiReader.java
@@ -12,12 +12,14 @@ public class MidiReader {
 	}
     }
 
-    public void readHeaderChunk() {
+    public boolean readHeaderChunk() {
 	try {
 	    char headID[] = {'M', 'T', 'h', 'd'};
 	    for (int i = 0; i < 4; i++) {
-		if ((char)input.readByte() != headID[i])
+		if ((char)input.readByte() != headID[i]) {
 		    System.err.println("Error reading header chunk. Not a valid Midi file.");
+		    return false;
+		}
 	    }
 	    headerChunkLength = input.readInt();
 	    fileFormat = input.readShort();
@@ -26,7 +28,9 @@ public class MidiReader {
 	} catch (IOException e) {
 	    System.err.println(e.toString());
 	}
+	return true;
     }
+
     public void printHeaderInformation() {
 	System.out.println("File Format: " + fileFormat);
 	System.out.println("Number of track chunks: " + trackCount);
